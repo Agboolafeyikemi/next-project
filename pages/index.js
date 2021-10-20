7601ab85a9e035302441cc38a296ee92bc91774f
Agboolafeyikemi <agboolafeyikemi93@gmail.com>
2021-10-21 00:25:54 +0100

    form

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,22 @@
-import Head from 'next/head'
-import Layout, { siteTitle } from './components/layouts'
-import utilStyles from './styles/utils.module.css'
+import Head from "next/head";
+import Layout, { siteTitle } from "./components/layouts";
+import utilStyles from "./styles/utils.module.css";
+import Form from "./components/Form";
 
 export default function Home() {
   return (
-    <Layout home >
-      <Head>
-        <title>{siteTitle}</title>
-      </Head>
-      <section className={utilStyles.headingMd}>
-        <p>Hello, My Name is Agboola Feyikemi. I am a Frontend Web Engineer experienced in building web technologies. I have over time worked on both fun and challenging projects.</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
-      </section>
-    </Layout>
-  )
+    // <Layout home >
+    //   <Head>
+    //     <title>{siteTitle}</title>
+    //   </Head>
+    //   <section className={utilStyles.headingMd}>
+    //     <p>Hello, My Name is Agboola Feyikemi. I am a Frontend Web Engineer experienced in building web technologies. I have over time worked on both fun and challenging projects.</p>
+    //     <p>
+    //       (This is a sample website - you’ll be building a site like this on{' '}
+    //       <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+    //     </p>
+    //   </section>
+    // </Layout>
+    <Form />
+  );
 }
