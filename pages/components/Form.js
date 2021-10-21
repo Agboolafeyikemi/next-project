38d5ff3fcdec46b13dd3723045f9bac10aae95ad
Agboolafeyikemi <agboolafeyikemi93@gmail.com>
2021-10-21 07:17:09 +0100

    form

--- pages/components/Form.js
+++ pages/components/Form.js
@@ -34,21 +34,23 @@ export default function Home() {
               data-netlify="true"
             >
               <input type="hidden" name="form-name" value="contact" />
-              <p>
-                <label htmlFor="name">Name</label>
-                <input type="text" id="name" name="name" />
-              </p>
-              <p>
-                <label htmlFor="email">Email</label>
-                <input type="text" id="email" name="email" />
-              </p>
-              <p>
-                <label htmlFor="message">Message</label>
-                <textarea id="message" name="message"></textarea>
-              </p>
-              <p>
-                <button type="submit">Send</button>
-              </p>
+              <div>
+                <p>
+                  <label htmlFor="name">Name</label>
+                  <input type="text" id="name" name="name" />
+                </p>
+                <p>
+                  <label htmlFor="email">Email</label>
+                  <input type="text" id="email" name="email" />
+                </p>
+                <p>
+                  <label htmlFor="message">Message</label>
+                  <textarea id="message" name="message"></textarea>
+                </p>
+                <p>
+                  <button type="submit">Send</button>
+                </p>
+              </div>
             </form>
           </div>
         </div>
